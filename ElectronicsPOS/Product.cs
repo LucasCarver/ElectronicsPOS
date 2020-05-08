0ebb213cfff83390a26f8882639b7d8d1a69fc1e
LucasCarver <carver.lucas@gmail.com>
2020-05-08 13:42:00 -0400

    Added product class

---
+++ ElectronicsPOS/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronicsPOS
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public Product(string name, string category, string description, double price)
+        {
+            Name = name;
+            Category = category;
+            Description = description;
+            Price = price;
+        }
+        public Product() { }
+    }
+}
