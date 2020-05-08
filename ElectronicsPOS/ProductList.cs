3d39083ab40c7d8238f92303a11ec29765259c49
LucasCarver <carver.lucas@gmail.com>
2020-05-08 15:56:57 -0400

    ProductList.cs added

---
+++ ElectronicsPOS/ProductList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ElectronicsPOS
+{
+    class ProductList
+    {
+        public List<Product> Products { get; set; }
+        public static void ProductFileToList(List<Product> products)
+        {
+            string[] productsArray;
+            StreamReader reader = new StreamReader("../../Product_List.txt");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                productsArray = line.Split('|');
+                products.Add(new Product(productsArray[0], productsArray[1], productsArray[2], double.Parse(productsArray[3])));
+                line = reader.ReadLine();
+            }
+            reader.Close();
+        }
+    }
+}
