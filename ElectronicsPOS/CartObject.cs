a3bc07fd7fd7bae57ed463f317a4f3615a004a36
LucasCarver <carver.lucas@gmail.com>
2020-05-08 17:09:06 -0400

    Cart and CartObject added.

---
+++ ElectronicsPOS/CartObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronicsPOS
+{
+    class CartObject
+    {
+        public Product Item{ get; set; }
+        public int ItemQuantity { get; set; }
+        public CartObject(Product item, int itemQuantity)
+        {
+            Item = item;
+            ItemQuantity = itemQuantity;
+        }
+    }
+}
