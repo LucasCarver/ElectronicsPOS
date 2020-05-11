b9be3b05525de99fde02a0cefe592be7c2ed5127
james <jamestheodorewatkins@gmail.com>
2020-05-11 10:35:06 -0400

    Adding View classes

---
+++ ElectronicsPOS/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronicsPOS
+{
+    class Menu
+    {
+        public static void PrintMenu(List<Product> productList)
+        {
+            int index = 1;
+
+            foreach(Product p in productList)
+            {
+                Console.Write($"{index}. ");
+                DisplayProduct.DrawProduct(p);
+                Console.WriteLine("=============================");
+                index++;
+
+            }
+        }
+    }
+}
